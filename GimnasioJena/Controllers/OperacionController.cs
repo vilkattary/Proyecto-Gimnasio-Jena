f24506bf31a21b71bf7f29629bbcc94d65c0adc4
Sunmy Serrano <sunmyserrano85@gmail.com>
2026-04-14 17:48:41 -0600

    Operaciones

---
+++ GimnasioJena/Controllers/OperacionController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GimnasioJena.Controllers
+{
+    public class OperacionController : Controller
+    {
+        // GET: Operacion
+        public ActionResult Operaciones()
+        {
+            return View();
+        }
+        public ActionResult ListaAsistencia()
+        {
+            return View();
+        }
+        public ActionResult PanelMensajes()
+        {
+            return View();
+        }
+        public ActionResult MisClases()
+        {
+            return View();
+        }
+    }
+}
+
