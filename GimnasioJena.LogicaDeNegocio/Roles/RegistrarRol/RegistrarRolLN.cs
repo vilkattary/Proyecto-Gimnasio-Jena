c51daa343c769974c39d8fc0b5abb8417fa8f14c
lolaodiorodriguez <fabiolaodio777@hotmail.com>
2026-05-25 20:17:52 -0600

    Cambios Eder
    Nueva estructura

---
+++ GimnasioJena.LogicaDeNegocio/Roles/RegistrarRol/RegistrarRolLN.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GimnasioJena.LogicaDeNegocio.Roles.RegistrarRol
+{
+    public class RegistrarRolLN
+    {
+
+    }
+}
---
+++ GimnasioJena.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GimnasioJena.UI.MvcApplication" Language="C#" %>
