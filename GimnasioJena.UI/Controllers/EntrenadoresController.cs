02c754180cae4c4aada043fd5dd30ede72016a3a
lolaodiorodriguez <fabiolaodio777@hotmail.com>
2026-06-15 20:31:20 -0600

    Vista del perfil del entrenador

--- GimnasioJena.UI/Controllers/EntrenadoresController.cs
+++ GimnasioJena.UI/Controllers/EntrenadoresController.cs
@@ -1,100 +1,34 @@
-﻿using GimnasioJena.Abstracciones.LogicaDeNegocio.Usuarios.ObtenerUsuarioPorId;
+﻿using GimnasioJena.Abstracciones.LogicaDeNegocio.Entrenadores.ObtenerEntrenadorPorId;
+using GimnasioJena.Abstracciones.Modelos.Entrenadores;
+using GimnasioJena.AccesoADatos.Entrenadores.ObtenerEntrenadorPorId;
+using GimnasioJena.LogicaDeNegocio.Entrenadores.ObtenerEntrenadorPorId;
 using Microsoft.AspNet.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+
 
 namespace GimnasioJena.UI.Controllers
 {
     [Authorize(Roles = "ENTRENADOR")]
     public class EntrenadoresController : Controller
     {
-        private readonly IObtenerUsuarioPorIdLN _obtenerUsuarioServicio;
+        private readonly IObtenerEntrenadorPorIdLN _obtenerEntrenadorServicio;
 
-        public EntrenadoresController(IObtenerUsuarioPorIdLN obtenerUsuarioServicio)
+        public EntrenadoresController()
         {
-            _obtenerUsuarioServicio = obtenerUsuarioServicio;
+            var repositorio = new ObtenerEntrenadorPorIdAD();
+            _obtenerEntrenadorServicio = new ObtenerEntrenadorPorIdLN(repositorio);
+
         }
-        // GET: Entrenador
-        public ActionResult Index()
+
+        public async Task<ActionResult> Index()
         {
             var identityUserId = User.Identity.GetUserId();
-            var perfil = await _obtenerUsuarioServicio.ObtenerEntrenadorPorId(identityUserId);
+
+            var perfil = await _obtenerEntrenadorServicio.ObtenerEntrenadorPorId(identityUserId);
             return View(perfil);
+
         }
 
-        // GET: Entrenador/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Entrenador/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Entrenador/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Entrenador/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Entrenador/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Entrenador/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Entrenador/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
