a0283dddbea66a42bb1bfd6068593e5131a2620d
Ed3R08 <ederseva@gmail.com>
2026-06-19 18:02:48 -0600

    Registro de asistestencia por clase

--- GimnasioJena.Abstracciones/LogicaDeNegocio/Asistencias/RegistrarAsistencia/IRegistrarAsistenciaLN.cs
+++ GimnasioJena.Abstracciones/LogicaDeNegocio/Asistencias/RegistrarAsistencia/IRegistrarAsistenciaLN.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GimnasioJena.Abstracciones.Modelos.Asistencias;
 
 namespace GimnasioJena.Abstracciones.LogicaDeNegocio.Asistencias.RegistrarAsistencia
 {
     public interface IRegistrarAsistenciaLN
     {
+        bool RegistrarAsistencia(AsistenciaCrearDto asistencia);
     }
 }
--- GimnasioJena.LogicaDeNegocio/Asistencias/RegistrarAsistencia/RegistrarAsistenciaLN.cs
+++ GimnasioJena.LogicaDeNegocio/Asistencias/RegistrarAsistencia/RegistrarAsistenciaLN.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GimnasioJena.Abstracciones.AccesoADatos.Asistencias.RegistrarAsistencia;
+using GimnasioJena.Abstracciones.LogicaDeNegocio.Asistencias.RegistrarAsistencia;
+using GimnasioJena.Abstracciones.Modelos.Asistencias;
+using GimnasioJena.AccesoADatos.Asistencias.RegistrarAsistencia;
 
 namespace GimnasioJena.LogicaDeNegocio.Asistencias.RegistrarAsistencia
 {
-    public class RegistrarAsistenciaLN
+    public class RegistrarAsistenciaLN : IRegistrarAsistenciaLN
     {
+        private readonly IRegistrarAsistenciaAD _registrarAsistenciaAD;
+
+        public RegistrarAsistenciaLN()
+        {
+            _registrarAsistenciaAD = new RegistrarAsistenciaAD();
+        }
+
+        public bool RegistrarAsistencia(AsistenciaCrearDto asistencia)
+        {
+            if (asistencia == null)
+                return false;
+
+            if (asistencia.idReserva <= 0)
+                return false;
+
+            return _registrarAsistenciaAD.RegistrarAsistencia(asistencia) > 0;
+        }
     }
 }
